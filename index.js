33da4fb1218e51159ddff38db3f722b6d5f5629f
devil-cyber <mani2474695@gmail.com>
2020-05-08 14:07:16 +0530

    Create the server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Heloo")
+const express= require('express');
+const app=express();
+const fs = require('fs');
+const multer = require('multer');
+const {TesseractWorker} =require('tesseract.js')
+const worker = new TesseractWorker();
+
+
+
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, res, cb) {
+      cb(null, req.file)
+    }
+  });
+   
+  var upload = multer({ storage: storage }).single('avatar');
