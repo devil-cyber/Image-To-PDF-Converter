d810f582d96b93d7abcbeb31db782ae5e669aba9
devil-cyber <mani2474695@gmail.com>
2020-05-09 12:08:19 +0530

    Image to pdf

--- index.js
+++ index.js
@@ -2,40 +2,52 @@ const express= require('express');
 const app=express();
 const fs = require('fs');
 const multer = require('multer');
-// const { TesseractWorker } = require("tesseract.js");
-// const worker = new TesseractWorker();
 
 
-const { createWorker } =require( 'tesseract.js');
-
-const worker = createWorker({
-  logger: m => console.log(m)
+const {TesseractWorker}=require('tesseract.js');
+const worker = new TesseractWorker();
+const storage =multer.diskStorage({
+  destination:(req,file,cb)=>{
+      cb(null,"./uploads");
+  },
+  filename:(req,file,cb)=>{
+   cb(null,file.originalname)
+  }
 });
- 
-
-
-
-
-
-let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads')
-    },
-    filename: function (req, res, cb) {
-      cb(null, req.file)
-    }
-  });
-   
-  var upload = multer({ storage: storage }).single('avatar');
+const upload = multer({ storage: storage }).single('avtar');
 
 app.set('view engine','ejs');
+app.use(express.static("public"));
 
 
 app.get('/',function(req,res){
   return res.render('index');
 })
-
-
+app.post('/uploads',function(req,res){
+upload(req,res,err =>{
+  fs.readFile(`./uploads/${req.file.originalname }`,(err,data)=>{
+    if(err){
+      return console.log("Error",err);
+    }
+    worker
+    .recognize(data,"eng",{tessjs_create_pdf:'1'})
+    .progress(progress=>{
+      console.log(progress);
+    })
+    .then(result=>{
+      res.redirect("/download");
+    })
+    .finally(()=> worker.terminate());
+  })
+  console.log(req.file);
+})
+console.log("hello");
+})
+app.get("/download",function(req,res){
+const file=`${__dirname}/tesseract.js-ocr-result.pdf`;
+console.log("Hello");
+res.download(file);
+})
 
 const port=5000 || process.env.PORT;
 app.listen(port,function(err){
